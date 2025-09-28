844321dc42afdf3db95a58cf72969e0aa179c9df
captivecow <captivecow.dev@gmail.com>
2025-09-28 15:07:18 -0500

    feat: setup with css normalize

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { Application, Assets, Container, Sprite } from 'pixi.js';
+
+(async () => {
+  const app = new Application();
+  await app.init({ background: '#1099bb', resizeTo: window });
+  document.body.appendChild(app.canvas);
+})();
+
